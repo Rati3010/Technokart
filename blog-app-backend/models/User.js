f070736adef8a078c1575677d783827d3b212807
Rati3010 <mratikanta1997@gmail.com>
2024-07-26 09:40:12 +0530

    created the comment schema and model

--- blog-app-backend/models/User.js
+++ blog-app-backend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: { type: String, require: true },
   email: { type: String, require: true, unique: true },
   password: { type: String, require: true },
